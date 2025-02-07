5401f5d4ca80963641d1312a8a378c94774bda53
BenAndrews05 <ba467@exeter.ac.uk>
2025-02-07 17:16:02 +0000

    added account database table
    added a account table to the database. We are on database version 0002  for accounts. can check by running py manage.py showmigrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class account(models.Model):
+    userId = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    points = models.IntegerField()
+    accountType = models.CharField(max_length=10)
+    # profile picture
+
+class tasks(models.Model):
+    taskID = models.IntegerField(primary_key=True)
+    #tasks 
