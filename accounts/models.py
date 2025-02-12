7417f626df5a354d456d575bf639129a4b9ea300
Iestyn Perkins <iestyn.perkins1205@gmail.com>
2025-02-12 12:53:10 +0000

    trying to make points work
    WIP, not finished

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class account(models.Model):
-    userId = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     points = models.IntegerField(default=0)
-    accountType = models.CharField(max_length=10)
-    # profile picture
 
 class tasks(models.Model):
     taskID = models.IntegerField(primary_key=True)
