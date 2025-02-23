463945cbdd560ae791d6f91d691da39b37b2fe30
Iestyn Perkins <iestyn.perkins1205@gmail.com>
2025-02-23 19:22:58 +0000

    GDPR compliance (i hope)

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,5 +12,8 @@ urlpatterns = [
     path("search/", views.search_users, name="search_users"),
     path("profile/", views.profile_view, name="profile"),
     path("profile/<str:username>/", views.profile_view, name="profile_with_username"),
+    path("delete-account/", views.delete_account, name="delete_account"),
+    path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
+    path("download-data/", views.download_data, name="download_data"),
 ]
 
