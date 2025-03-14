4a4f7db3f91b412699d8821b12a65edf69e8021f
BenAndrews05 <ba467@exeter.ac.uk>
2025-03-14 12:53:40 +0000

    info page styling

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p_^s$58^4_o*%)zxi^!p(e5h$5q&r#$+c9ksgn%8@y8y6cdn%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.10.0.12","127.0.0.1"]
+ALLOWED_HOSTS = ["10.207.137.196","127.0.0.1"]
 
 
 # Application definition
